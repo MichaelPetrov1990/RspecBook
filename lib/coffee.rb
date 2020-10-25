8ad85f20770d678275ba3225a901e5b3d68d846a
Michael Petrov <mpetrovcello@gmail.com>
2020-10-25 18:34:02 +0000

    tea parties are jolly

--- lib/coffee.rb
+++ lib/coffee.rb
@@ -8,4 +8,10 @@ class Coffee
   def price
     1.00 + ingredients.length * 0.25
   end
+  def color
+    ingredients.include?(:milk) ? :light : :dark
+  end
+  def temperature
+    ingredients.include?(:milk) ? 190.0 : 205.0
+  end
 end
--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -8,10 +8,19 @@ RSpec.describe 'a cup of coffee' do
   it 'costs $1.00' do
     expect(coffee.price).to eq(1.00)
   end
+  
   context 'with milk' do
-    before {coffee.add :mik}
+    before {coffee.add :milk}
     it 'costs $1.25' do
       expect(coffee.price).to eq(1.25)
+    end
+    it 'is light in color' do
+    
+      expect(coffee.color).to be(:light)
+    end
+    it 'is cooler than 200 degrees Fahrenheit' do
+      
+      expect(coffee.temperature).to be < 200.0
     end
   end
 end
